10ae936aadb2369f1cc2ebcd6424d0130405858e
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-23 22:00:55 -0500

    Add project files.

---
+++ StoreBookWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StoreBookWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
